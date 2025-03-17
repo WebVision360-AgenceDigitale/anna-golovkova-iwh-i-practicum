6b3fd0604d35913da45e89dd173a20e2e49e5849
Anna <anna@webvision360.com>
2025-03-17 21:28:30 +0200

    Endpoint for record creation added + error displaying added to record creation form

--- index.js
+++ index.js
@@ -17,17 +17,51 @@ if (!process.env.CUSTOM_OBJECT_ID) {
     throw new Error('CUSTOM_OBJECT_ID environment variable should be set');
 }
 
+const hsClient = axios.create({
+    baseURL: 'https://api.hubspot.com/',
+    timeout: 5000,
+    headers: {
+        Authorization: `Bearer ${ process.env.HS_API_KEY }`,
+        'Content-Type': 'application/json',
+    },
+});
+
 // app.get('/', () => {
 // });
 
 app.get('/update-cobj', async (req, res) => {
     res.render('updates', {
         title: 'Update Custom Object Form | Integrating With HubSpot I Practicum',
+        error: req.query.error || false,
     });
 });
 
-// app.post('/update-cobj', () => {
-// });
+app.post('/update-cobj', async (req, res) => {
+    try {
+        const resp = await hsClient.post(
+            `crm/v3/objects/${ process.env.CUSTOM_OBJECT_ID }`,
+            {
+                properties: {
+                    name: req.body.name,
+                    age: req.body.age,
+                    breed: req.body.breed,
+                    gender: req.body.gender,
+                }
+            },
+        );
+        const data = resp.data;
+    
+        if (data?.id) {
+            res.redirect('/');
+        } else {
+            throw new Error('Something went wrong');
+        }
+    }
+    catch (e) {
+        console.error(e);
+        res.redirect(`/update-cobj?error=${ encodeURIComponent(e.response?.data?.message || e.message || 'Something went wrong') }`);
+    }
+});
 
 // * Localhost
 const port = process?.env?.PORT || 3000;
